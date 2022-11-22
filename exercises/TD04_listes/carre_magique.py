2e9ecf509399802b70df2facc315e0435631cede
Santiago VARGAS-TRIANA <santiago.vargas-triana@ens.uvsq.fr>
2022-11-22 14:04:50 +0100

    correction

--- exercises/TD04_listes/carre_magique.py
+++ exercises/TD04_listes/carre_magique.py
@@ -1,4 +1,16 @@
-carre_mag = []
-for i in range (4):
-    ligne_1 = []
-    
+carre_mag = [
+ [4,  14,  15, 1],
+ [9, 7, 6,  12],
+ [5,  11,  10, 8],
+ [ 16, 2, 3,  13]
+]
+for i in range(len(carre_mag)):
+    for j in range(len(carre_mag[i])):
+        print(carre_mag[i][j], end=" ")
+    print()
+
+carre_pas_mag = carre_mag
+carre_pas_mag[3][2]= 7
+print (carre_pas_mag)
+
+print(len(carre_mag))
